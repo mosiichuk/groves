def141fbb01bb8951b44a8149706be43aaca4aff
Yevhen Mosiichuk <zhenya9801@gmail.com>
2020-12-05 20:20:27 +0200

    Bunch of fixes #2

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-    initIframe();
-});
-
-window.addEventListener('resize', () => {
-    initIframe();
-})
-
-function initIframe() {
-    const trackIFrame = document.getElementById("whole-track");
-    trackIFrame.style.height = trackIFrame.clientWidth * 1.35 + 'px';
-
-    const albumIFrame = document.getElementById("cherry-album");
-    albumIFrame.style.height = albumIFrame.clientWidth * 1.9 + 'px';
-}
+// document.addEventListener("DOMContentLoaded", () => {
+//     initIframe();
+// });
+//
+// window.addEventListener('resize', () => {
+//     initIframe();
+// })
+//
+// function initIframe() {
+//     const trackIFrame = document.getElementById("whole-track");
+//     trackIFrame.style.height = trackIFrame.clientWidth * 1.35 + 'px';
+//
+//     const albumIFrame = document.getElementById("cherry-album");
+//     albumIFrame.style.height = albumIFrame.clientWidth * 1.9 + 'px';
+// }
